ea0b1c984683ea6fbd96df5991b37a79c5c787d2
Emilio Alvarez <emi.alv@outlook.com>
2020-09-02 16:37:01 -0600

    Formatted positions.

--- script.js
+++ script.js
@@ -18,17 +18,24 @@ $(document).ready(function () {
 
 function populateExperience(experience) {
   let html = ejs.render(
-    `<% for (position of experience) { %>
-    <div class="position">
-    <a href="<%= position.link %>" class="position__company"><%= position.company %></a>
-    <div class="position__title"><%= position.title %></div>
-    <div class="position__location"><%= position.location %></div>
-    <div class="position__date"><%= position.dateString %></div>
-    <ul class="position__duties">
-    <% for (duty of position.duties) { %>
-    <li><%= duty %></li>
-    <% } %>
-    </ul>
+    `
+    <% for (position of experience) { %>
+    <div class="position" style="border-left-color: <%= position.color %>">
+      <div class="position__header">
+        <a href="<%= position.link %>" class="position__company link--invisible">
+          <%= position.company %>
+        </a>
+        <span class="position__location"><%= position.location %></span>
+      </div>
+      <div class="position__header">
+        <div class="position__title"><%= position.title %></div>
+        <div class="position__date"><%= position.dateString %></div>
+      </div>
+      <ul class="position__duties">
+        <% for (duty of position.duties) { %>
+        <li><%= duty %></li>
+        <% } %>
+      </ul>
     </div>
     <% } %>`,
     { experience }
@@ -38,13 +45,14 @@ function populateExperience(experience) {
 
 function populateProjects(projects) {
   let html = ejs.render(
-    `<% for (project of projects) { %>
+    `
+    <% for (project of projects) { %>
     <div class="project">
-    <div class="project__name"><%= project.name %></div>
-    <div class="project__shortDescription"><%= project.shortDescription %></div>
-    <% if (project.github) { %>
-    <a href="<%= project.github %>" class="project__github">Source code</a>
-    <% } %>
+      <div class="project__name"><%= project.name %></div>
+      <div class="project__shortDescription"><%= project.shortDescription %></div>
+      <% if (project.github) { %>
+      <a href="<%= project.github %>" class="project__github">Source code</a>
+      <% } %>
     </div>
     <% } %>`,
     { projects }
