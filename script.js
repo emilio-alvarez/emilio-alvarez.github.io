b076889572769d2910405b4c717219587adbb3b6
Emilio Alvarez <emi.alv@outlook.com>
2020-09-02 10:30:15 -0600

    Using ejs to render experience/projects.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const experienceJSON = "content/experience.json";
+const projectsJSON = "content/projects.json";
 
 $(document).ready(function () {
   // Load projects.json
@@ -7,16 +8,46 @@ $(document).ready(function () {
       populateExperience(experience);
     }
   });
+
+  $.getJSON(projectsJSON, (projects) => {
+    if (Array.isArray(projects)) {
+      populateProjects(projects);
+    }
+  });
 });
 
 function populateExperience(experience) {
   let html = ejs.render(
     `<% for (position of experience) { %>
-    <h1><%= position.company %></h1>
+    <div class="position">
+    <a href="<%= position.link %>" class="position__company"><%= position.company %></a>
+    <div class="position__title"><%= position.title %></div>
+    <div class="position__location"><%= position.location %></div>
+    <div class="position__date"><%= position.dateString %></div>
+    <ul class="position__duties">
+    <% for (duty of position.duties) { %>
+    <li><%= duty %></li>
+    <% } %>
+    </ul>
+    </div>
     <% } %>`,
     { experience }
   );
   $("#experience-container").html(html);
 }
 
-function generateProjectElement(projects) {}
+function populateProjects(projects) {
+  let html = ejs.render(
+    `<% for (project of projects) { %>
+    <div class="project">
+    <div class="project__name"><%= project.name %></div>
+    <div class="project__shortDescription"><%= project.shortDescription %></div>
+    <% if (project.github) { %>
+    <a href="<%= project.github %>" class="project__github">Source code</a>
+    <% } %>
+    </div>
+    <% } %>`,
+    { projects }
+  );
+  $("#projects-container").html(html);
+}
